55fd32b1d808326ebbb446ce25892c5198f0a713
FazaDR <rafsanjanidipa@gmail.com>
2026-07-25 15:10:33 +0700

    localization is removed

--- src/mapping/src/mapping_node.cpp
+++ src/mapping/src/mapping_node.cpp
@@ -1,5 +1,8 @@
 #include "mapping/mapping_node.hpp"
 
+
+#include <functional>
+
 MappingNode::MappingNode()
 : Node("mapping_node")
 {
@@ -10,6 +13,11 @@ MappingNode::MappingNode()
             &MappingNode::pointCloudCallback,
             this,
             std::placeholders::_1));
+
+    gz_node_.Subscribe(
+        "/world/winding_room/dynamic_pose/info",
+        &MappingNode::poseCallback,
+        this);
 
     RCLCPP_INFO(get_logger(), "Mapping node started.");
 }
@@ -23,4 +31,24 @@ void MappingNode::pointCloudCallback(
         msg->header.frame_id.c_str(),
         msg->width,
         msg->height);
+}
+
+void MappingNode::poseCallback(const gz::msgs::Pose_V &msg)
+{
+    for (int i = 0; i < msg.pose_size(); ++i)
+    {
+        const auto &pose = msg.pose(i);
+
+        if (pose.name() == "ddmr")
+        {
+            RCLCPP_INFO(
+                get_logger(),
+                "Robot | x=%.3f y=%.3f z=%.3f",
+                pose.position().x(),
+                pose.position().y(),
+                pose.position().z());
+
+            break;
+        }
+    }
 }
